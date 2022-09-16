38a300604a3204f4655995e014f5a67811ab9624
Nikolai <ezzik13@mail.ru>
2022-09-16 15:08:56 +0500

    Создал программу на нахождение вещественного  частного

---
+++ Example_004_Div/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти частное
+
+using static System.Console;
+
+Clear();
+WriteLine("Введите 2 числа: ");
+double num1 = Convert.ToInt32(ReadLine());
+double num2 = Convert.ToInt32(ReadLine());
+double Div1 = num1 / num2;
+WriteLine($"Частное {num1} и {num2} = {Div1}");
